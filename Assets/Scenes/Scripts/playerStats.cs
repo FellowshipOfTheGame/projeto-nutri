1cb55187469c2832d56645b832d14d6beb3592f1
Sincaruk2018 <sincaruk@usp.br>
2021-11-06 16:12:48 -0300

    virtual JStick

--- Assets/Scenes/Scripts/playerStats.cs
+++ Assets/Scenes/Scripts/playerStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,11 +6,11 @@ using UnityEngine;
 
 public class playerStats : MonoBehaviour
 {
-    private int points;
-    private int barPoint;
-    private float multPoint;
+    private static int points;
+    private static int barPoint;
+    private static int multPoint;
 
-    public int Points
+    /*public int Points
     {
         get => points;
         set => points = value;
@@ -25,20 +26,47 @@ public class playerStats : MonoBehaviour
     {
         get => multPoint;
         set => multPoint = value;
-    }
+    }*/
 
 
     // Start is called before the first frame update
     void Start()
     {
-        this.points = 0;
-        this.barPoint = 0;
-        this.multPoint = 0f;
+        points = 0;
+        barPoint = 0;
+        multPoint = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void AddPoint() // Adds points accordingly to the mult value
+    {
+        points += multPoint;
+        barPoint++;
+        if (barPoint > 8)
+        {
+            multPoint++;
+            if (multPoint <= 4)
+            {
+                barPoint = 0;
+            }
+            barPoint = Mathf.Clamp(barPoint,0,8);
+            multPoint = Mathf.Clamp(multPoint,1,4);
+        }
+    }
+
+    public static void ResetStreak()
+    {
+        barPoint = 0;
+        multPoint = 1;
+    }
+
+    public static void SoftReset() //for tests only
+    {
+        barPoint = 0;
     }
 }
