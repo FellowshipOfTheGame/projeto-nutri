cfc6163f5d3e163e548b90b6ef5703b9dce8db53
Óliver S Becker <oliverbecker@usp.br>
2022-01-30 19:47:36 -0300

    Implemented movement animation logic

--- Assets/FoG/Scripts/PewController.cs
+++ Assets/FoG/Scripts/PewController.cs
@@ -7,23 +7,76 @@ using UnityEngine.InputSystem.Utilities;
 
 public class PewController : MonoBehaviour
 {
-    public Gamepad gp;
-    public Rigidbody2D rb;
-
     [SerializeField]
     public float playerVelocity;
 
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string walkingAnimatorName = "IsWalking";
+
+    private PlayerControl playerControl;
+    private Rigidbody2D rb;
+
+    private Vector2 moveInput;
+    private Vector3 scale;
+
+    bool isFlipped = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        gp = Gamepad.current;
+        playerControl = new PlayerControl();
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 moving = gp.leftStick.ReadValue();
-        rb.velocity = moving * playerVelocity;
+        Move();
+    }
+
+    void OnEnable()
+    {
+        playerControl.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerControl.Player.Disable();
+    }
+
+    void Move()
+    {
+        moveInput = playerControl.Player.Move.ReadValue<Vector2>();
+
+        if (moveInput.magnitude > 0)
+        {
+            animator.SetBool(walkingAnimatorName, true);
+            rb.velocity = moveInput * playerVelocity;
+
+            if (moveInput.x > 0 && !isFlipped)
+            {
+                FlipPlayer();
+            }
+            else if (moveInput.x < 0 && isFlipped)
+            {
+                FlipPlayer();
+            }
+        }
+        else
+        {
+            animator.SetBool(walkingAnimatorName, false);
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void FlipPlayer()
+    {
+        isFlipped = !isFlipped;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
