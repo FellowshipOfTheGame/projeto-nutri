09a8b6fb77a5c8e02551560ecfbc33994da54c00
Sincaruk <sincaruk@usp.br>
2022-12-28 19:40:32 -0300

    Precisa ajustar angulação e posição

--- Assets/arrowScript.cs
+++ Assets/arrowScript.cs
@@ -6,6 +6,10 @@ public class arrowScript : MonoBehaviour
 {
     public GameObject beetle;
     public GameObject player;
+    public RectTransform myRectTransform;
+    public Camera mainCamera;
+    Vector3 scrToWrldPointBeetle;
+
     float aux = 0.1f;
     //public Camera cam;
     // Start is called before the first frame update
@@ -13,7 +17,10 @@ public class arrowScript : MonoBehaviour
     {
         beetle = GameObject.Find("FinishLine");
         player = GameObject.FindWithTag("Player");
-        //camera = Camera.main;
+        myRectTransform = gameObject.GetComponent<RectTransform>();
+        mainCamera = Camera.main;
+        //scrToWrldPointBeetle = mainCamera.WorldToScreenPoint(beetle.transform.position);
+
     }
 
     // Update is called once per frame
@@ -22,9 +29,34 @@ public class arrowScript : MonoBehaviour
         Debug.DrawLine(beetle.transform.position,player.transform.position,Color.red,0f,false);
         float auxM = beetle.transform.position.x - player.transform.position.x;
         float auxA = beetle.transform.position.y - player.transform.position.y;
+        /*float quickfixangles = 1.0f;
+        if(auxM > 0.0f && ) // First Quadrant
+        {
+
+        }
+        else if() // Se
+        {
+
+        }*/
+
+
         auxA = auxA/auxM;
-        auxA = Mathf.Atan(auxA);
+        //auxA = Mathf.Atan(auxA) + quickfixangles;
         auxA = auxA * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f,0f, auxA -90.0f); // ABNER, I USED THIS BECAUSE MY ARROW DRAWING WAS POINTING UP
+
+        /* Change arrow position*/
+        Vector3 scrToWrldPointBeetle = mainCamera.WorldToScreenPoint(beetle.transform.position);
+        //Vector3 camBoundary = mainCamera.ScreenToWorldPoin(  );
+        float margin = 40.0f;
+        float xArrow = scrToWrldPointBeetle.x;
+        xArrow = Mathf.Clamp(xArrow,((-Screen.width)/2)+margin,((Screen.width)/2) -margin);
+        float yArrow = scrToWrldPointBeetle.y;
+        yArrow = Mathf.Clamp(yArrow,((-Screen.height)/2)+margin,((Screen.height)/2) -margin);
+
+        Debug.Log("X: " + xArrow + "Y: " + yArrow);
+
+        myRectTransform.anchoredPosition = new Vector2 (xArrow,yArrow);
+        //transform.position = new Vector3(xArrow,yArrow,0f);
     }
 }
